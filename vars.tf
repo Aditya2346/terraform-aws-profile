badd9cd2de862271795e17a49831a06c9044465a
AdiDev-build <adityathombre@gmail.com>
2021-07-05 01:03:23 +0530

    Variables File Complete

---
+++ vars.tf
@@ -0,0 +1,108 @@
+variable "REGION" {
+  default = "us-east-2"
+}
+
+variable "ZONE1" {
+  default = "us-east-2a"
+}
+
+variable "ZONE2" {
+  default = "us-east-2b"
+}
+
+variable "ZONE3" {
+  default = "us-east-2c"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-03657b56516ab7912"
+    us-east-1 = "ami-0947d2ba12ee1ff75"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
+
+
+variable "PUB_KEY" {
+  default = "dovekey.pub"
+}
+
+variable "PRIV_KEY" {
+  default = "dovekey"
+}
+
+variable "MYIP" {
+  default = "152.57.182.19/32"
+}
+variable rmquser {
+  default = "rabbit"
+}
+
+variable rmqpass {
+  default = "Gr33n@pple123456"
+}
+
+variable dbuser {
+  default = "admin"
+}
+
+variable dbpass {
+  default = "admin123"
+}
+
+variable dbname {
+  default = "accounts"
+}
+
+variable instance_count {
+  default = "1"
+}
+
+variable VPC_NAME {
+  default = "vprofile-VPC"
+}
+
+variable Zone1 {
+  default = "us-east-2a"
+}
+
+variable Zone2 {
+  default = "us-east-2b"
+}
+
+variable Zone3 {
+  default = "us-east-2c"
+}
+
+variable VpcCIDR {
+  default = "172.21.0.0/16"
+}
+
+
+variable PubSub1CIDR {
+  default = "172.21.1.0/24"
+}
+
+variable PubSub2CIDR {
+  default = "172.21.2.0/24"
+}
+
+variable PubSub3CIDR {
+  default = "172.21.3.0/24"
+}
+
+variable PrivSub1CIDR {
+  default = "172.21.4.0/24"
+}
+
+variable PrivSub2CIDR {
+  default = "172.21.5.0/24"
+}
+
+variable PrivSub3CIDR {
+  default = "172.21.6.0/24"
+}
